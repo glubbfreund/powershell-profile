d9256f02add367b994b7d0a3b3d47380097e5320
Oliver Leis <oliver.leis@gmail.com>
2024-09-19 11:09:47 +0200

    initial upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+# OMS initialization and theme loading
+oh-my-posh init pwsh --config 'C:\Users\olive\Documents\WindowsPowerShell\theme\hotstick.minimal.omp.json' | Invoke-Expression
+# Terminal icon module for colored icons with ls
+Import-Module -Name Terminal-Icons
+# Import module for better navigation
+Import-Module -Name z
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
