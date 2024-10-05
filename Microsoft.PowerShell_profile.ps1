6f241dafc6b0a8f7ed5dfd119ce44a989c67d64f
Oliver Leis <oliver.leis@gmail.com>
2024-10-05 10:59:27 +0200

    restored ompsh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # OMS initialization and theme loading
-# oh-my-posh init pwsh --config 'C:\Users\olive\Documents\WindowsPowerShell\theme\kapsoules.omp.json' | Invoke-Expression
-# Starship
-Invoke-Expression (&starship init powershell)
+Invoke-Expression (&oh-my-posh init pwsh --config 'C:\Users\olive\Documents\WindowsPowerShell\theme\kapsoules.omp.json')
+# Starship (disabled)
+# Invoke-Expression (&starship init powershell)
 # Terminal icon module for colored icons with ls
 Import-Module -Name Terminal-Icons
 # Import module for better navigation
